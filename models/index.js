0ffea45a20fa290300d57963fc56bd45cf8ac360
sunbi <minorSunbi@gmail.com>
2021-08-06 22:24:48 +0900

    reverted

--- models/index.js
+++ models/index.js
@@ -1,22 +1,27 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 // const db = {};
-const initModels = require('./init-models')
+const initModels = require("./init-models");
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-const db = initModels(sequelize)
+const db = initModels(sequelize);
 // fs
 //   .readdirSync(__dirname)
 //   .filter(file => {
