ea50f35473bb930a2d9f885305aa6f52925ce7d5
sunbi <minorSunbi@gmail.com>
2021-09-04 21:28:08 +0900

    / 경로는 맨 마지막에 위치해야 함

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,13 @@ const delivery = require("./delivery");
 const product = require("./product");
 const user = require("./user");
 
-router.use("", (req, res, next) => {
-  return res.status(200).json({ message: "check success" });
-});
 router.use("/auth", auth);
 router.use("/seller", seller);
 router.use("/delivery", delivery);
 router.use("/product", product);
 router.use("/user", user);
+router.use("/", (req, res, next) => {
+  return res.status(200).json({ message: "check success" });
+});
 
 module.exports = router;
